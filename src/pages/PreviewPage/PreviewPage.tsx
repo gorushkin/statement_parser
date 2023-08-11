24ad94c2888e6be6db370e189cedefd2eb584dd9
gorushkin <gorushkin@gmail.com>
2023-08-11 13:15:20 +0300

    refactor: refactor

--- src/pages/PreviewPage/PreviewPage.tsx
+++ src/pages/PreviewPage/PreviewPage.tsx
@@ -1,15 +1,10 @@
 import { Container } from '@chakra-ui/react';
-import { useLocation } from 'react-router-dom';
 import { Preview } from 'src/widgets/Preview';
 
-const PreviewPage = () => {
-  const location = useLocation();
-  console.log('location: ', location);
-  return (
-    <Container maxW={'100%'}>
-      <Preview />
-    </Container>
-  );
-};
+const PreviewPage = () => (
+  <Container maxW={'100%'}>
+    <Preview />
+  </Container>
+);
 
 export { PreviewPage };
