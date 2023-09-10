390b5d096ade03586b98e6accfb6579b27293ff3
gorushkin <gorushkin@gmail.com>
2023-09-10 22:07:47 +0300

    feat: add xls parser

---
+++ src/shared/parser/index.ts
@@ -0,0 +1,3 @@
+import { Parser } from './parser';
+
+export const parser = new Parser();
---
+++ src/shared/parser/types.ts
@@ -0,0 +1,10 @@
+export type Settings = {
+  headers: {
+    end: number;
+    start: number;
+  };
+  rows: {
+    end: number;
+    start: number;
+  };
+};
